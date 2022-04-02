246b0c010f9a826646e5fee9dc9838cb11ec941c
Braeden <bvenne@my.bcit.ca>
2022-04-01 22:19:33 -0700

    Added a function to app.js that will remove all the copied divs from the copies section

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 //part one
 const selectOriginal = document.querySelectorAll('.original-container div')
+const copyContainer = document.querySelector('.copy-container')
 
 function copyOriginalsSection(cloneDiv) {
-    const copyContainer = document.querySelector('.copy-container')
     copyContainer.insertAdjacentHTML('afterbegin', cloneDiv.currentTarget.outerHTML)
 }
 
 selectOriginal.forEach(element => element.addEventListener('click', copyOriginalsSection))
 
+//part two
+const clearBtn = document.querySelector('button')
 
+function clearSection() {
+    while (copyContainer.firstChild) {
+        copyContainer.removeChild(copyContainer.firstChild)}
+}
 
-
+clearBtn.addEventListener('click', clearSection)
